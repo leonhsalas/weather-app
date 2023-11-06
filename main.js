d84ce58dc036ac74dccca994ac3a0153cfc010e3
Leon Nicolhas <leonnhsalas@gmail.com>
2023-11-06 14:01:26 -0600

    final commit wins

--- main.js
+++ main.js
@@ -1,13 +1,45 @@
 
 const apiKey = "adcd29fe6b837bf7e07e35f4e77fe5a4";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=adcd29fe6b837bf7e07e35f4e77fe5a4&units=metric&q=bangalore";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}}`);
-    var data = await response.json();
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-    console.log(data);
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block"
+        document.querySelector(".weather").style.display = "none"
+    }else{
+        var data = await response.json();
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = Math.round(data.main.humidity) + "%";
+        document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + " km/h";
+    
+        if(data.weather[0].main === "Clouds"){
+            weatherIcon.src = "clouds.png";
+        }
+        else if(data.weather[0].main === "Clear"){
+            weatherIcon.src = "clear.png";
+        }
+        else if(data.weather[0].main === "Rain"){
+            weatherIcon.src = "rain.png";
+        }
+        else if(data.weather[0].main === "Drizzle"){
+            weatherIcon.src = "drizzle.png";
+        }
+        else if(data.weather[0].main === "Mist"){
+            weatherIcon.src = "mist.png";
+        }
+    
+        document.querySelector(".weather").style.display = "block";
+    }
 }
 
 
-checkWeather();
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
